96770ad4fb8ea4e31d81687ea1d66ebf93642275
Tasnim Hmida <tasnim.hmida@horizon-tech.tn>
2024-08-13 11:40:24 +0100

    added custom input field

---
+++ lib/core/widgets/input_field.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:online_bike_shopping_appuntitled/core/colors/colors.dart';
+import 'package:online_bike_shopping_appuntitled/core/colors/colors.dart';
+import 'package:online_bike_shopping_appuntitled/core/colors/colors.dart';
+
+class InputField extends StatefulWidget {
+  final TextEditingController controller;
+  final String? inputTitle;
+  final String? hintText;
+  late void Function(String value)? onChanged;
+  final String? Function(String?)? validator;
+  final String? prefixIcon;
+  final int? maxLines;
+  final bool isPassword;
+
+  InputField({
+    super.key,
+    required this.controller,
+    this.hintText,
+    this.inputTitle,
+    this.validator,
+    this.onChanged,
+    this.prefixIcon,
+    this.maxLines,
+    FocusNode? focusNode,
+    this.isPassword = false,
+  });
+
+  @override
+  State<InputField> createState() => _TextFormFieldWidget();
+}
+
+class _TextFormFieldWidget extends State<InputField> {
+  bool show = false;
+  bool hidePassword = true;
+  bool validate = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        widget.inputTitle != null
+            ? Column(
+                children: [
+                  Text(widget.inputTitle!,
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey,
+                      )),
+                  SizedBox(height: 10.h),
+                ],
+              )
+            : Container(),
+        TextFormField(
+            controller: widget.controller,
+            cursorColor: AppConstants.cornflowerBlueColor,
+            obscureText: widget.isPassword
+                ? hidePassword
+                    ? true
+                    : false
+                : false,
+            maxLines: widget.maxLines ?? 1,
+            onEditingComplete: () {
+              FocusScope.of(context).nextFocus();
+            },
+            onTapOutside: (event) {
+              FocusScope.of(context).unfocus();
+            },
+            style: TextStyle(
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w500,
+              fontSize: 15.sp,
+              letterSpacing: widget.isPassword ? 1 : 0,
+            ),
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            decoration: InputDecoration(
+              hintText: widget.hintText,
+              hintStyle: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15.sp),
+              errorStyle: TextStyle(
+                  fontFamily: "Poppins",
+                  color: const Color(0xffF04438),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15.sp),
+              errorBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.r),
+                borderSide: const BorderSide(color: Colors.red),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.r),
+                borderSide: const BorderSide(color: AppConstants.cornflowerBlueColor),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.r),
+                borderSide: const BorderSide(color: AppConstants.cornflowerBlueColor),
+              ),
+              prefixIcon: widget.prefixIcon != null
+                  ? IconButton(
+                      icon: SvgPicture.asset(
+                        widget.prefixIcon!,
+                        height: 24.h,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          hidePassword = !hidePassword;
+                        });
+                      },
+                    )
+                  : null,
+              suffixIcon: widget.isPassword
+                  ? IconButton(
+                      icon: SvgPicture.asset(
+                        'assets/icons/hide_password_icon.svg',
+                        height: 24.h,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          hidePassword = !hidePassword;
+                        });
+                      },
+                    )
+                  : null,
+            ),
+            validator: widget.validator,
+            onChanged: widget.onChanged),
+      ],
+    );
+  }
+}
--- lib/presentation/auth/widgets/register_widget.dart
+++ lib/presentation/auth/widgets/register_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:online_bike_shopping_appuntitled/core/widgets/input_field.dart';
 
 class RegisterWidget extends StatefulWidget {
   const RegisterWidget({Key? key}) : super(key: key);
@@ -8,13 +9,20 @@ class RegisterWidget extends StatefulWidget {
 }
 
 class _RegisterWidgetState extends State<RegisterWidget> {
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            InputField(
+              controller: _controller,
+              hintText: 'dlskfjls',
+            )
+          ],
+        ),
       ),
     );
   }
