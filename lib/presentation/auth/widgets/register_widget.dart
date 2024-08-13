67593648e8a4bead7be4a0b3dbcf17c7c090310e
Tasnim Hmida <tasnim.hmida@horizon-tech.tn>
2024-08-13 11:09:38 +0100

    started working on register screen ui

---
+++ lib/presentation/auth/widgets/register_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RegisterWidget extends StatefulWidget {
+  const RegisterWidget({Key? key}) : super(key: key);
+
+  @override
+  _RegisterWidgetState createState() => _RegisterWidgetState();
+}
+
+class _RegisterWidgetState extends State<RegisterWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
